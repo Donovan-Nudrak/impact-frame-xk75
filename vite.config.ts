69123860858c8b73b2243c3076cb15f098ab2bb0
Donovan-Nudrak <251686002+Donovan-Nudrak@users.noreply.github.com>
2026-09-24 14:20:54 +0000

    fix: use root base for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  base: "/impact-frame-xk75/",
+  base: "/",
   plugins: [react()],
   test: {
     environment: "jsdom",
